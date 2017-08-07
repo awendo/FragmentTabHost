1659dabb058b0f7e1acc913b5ac454a2dc972a63
awendo <hongsheng9311@163.com>
2017-08-07 15:05:16 +0800

    Initial commit

---
+++ app/src/main/java/com/awen/fragmenttabhost/Fragment3.java
@@ -0,0 +1,21 @@
+package com.awen.fragmenttabhost;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+
+public class Fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item3, null);
+        return view;
+    }
+}
